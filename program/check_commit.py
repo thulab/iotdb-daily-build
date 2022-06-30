9bbc00884299de9f60f65f8f1c0a184cd0826439
xiaoyekanren <876670773@qq.com>
2022-06-30 16:46:25 +0800

    添加检查commit程序

--- program/check_commit.py
+++ program/check_commit.py
@@ -1,23 +1,39 @@
 # coding=utf-8
+import sys
 from src import connect
 
 
-def check_commit():
-    pass
+# ----------拿到以下三个值的方式
+# commit_id: git log  -n 1 | head -n 1  | cut -d ' ' -f 2
+# commit_time: git log -n1 --pretty='format:%cd' --date=iso
+# branch: git branch
+# ----------样例
+# python3 check_commit.py 'query' '/Users/zhangzhengming/Src/Python/iotdb-daily-build-dependence/db/db.db' 'rel/0.12' '34b6a79913d7754f3f8397f9c26bdbef3a6393b5'
+
+
+def check_commit(cur_branch, db_path):
+    sql = 'select branch,commit_id,commit_time from commit_last where branch=\'%s\'' % cur_branch
+    return connect.select_db(sql, db_path)
+
+
+def update_commit(cur_branch, cur_commit_id, cur_commit_time, db_path):
+    sql = 'update commit_last set commit_id=\'%s\',commit_time=\'%s\' where branch=\'%s\'' % (cur_commit_id, cur_commit_time, cur_branch)
+    connect.insert_db(sql, db_path)
 
 
 if __name__ == '__main__':
-
+    if len(sys.argv[1:]) != 4:
+        print('必须设置4个参数')
+        exit()
+    dbpath, branch, commit_id, commit_time = sys.argv[1:]
     dbpath = '/Users/zhangzhengming/Src/Python/iotdb-daily-build-dependence/db/db.db'
-    try:
-        a, b = connect.select_db('select id,time from last_commit limit 1', dbpath)
-    except:
-        a, b = 0, 1
-    print(a, b)
-    if a:
-        print(123)
-        print(type(a))
-        print(len(str(a)))
-    else:
-        print(456)
-    # insert_db('insert into last_commit (id,time) values (\'%s\', \'%s\')' % ('abcdef', '2008-12-26'), dbpath)
+
+    db_branch, db_commit_id, db_commit_time = check_commit(branch, dbpath)
+
+    if branch == db_branch and commit_id == db_commit_id:
+        print('stop')
+        exit()
+    elif commit_id != db_commit_id:
+        update_commit(branch, commit_id, commit_time, dbpath)
+        print('update')
+        exit()
