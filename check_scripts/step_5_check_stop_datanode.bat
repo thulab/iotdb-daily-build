f17b45491779012fa3f0c6a6b6d2993e958736ba
xiaoyekanren <876670773@qq.com>
2022-12-09 13:47:55 +0800

    改第一个check v12

--- check_scripts/step_5_check_stop_datanode.bat
+++ check_scripts/step_5_check_stop_datanode.bat
@@ -1,7 +1,7 @@
 @echo off
-echo "start"
+echo "start" %ERRORLEVEL%
 set superior_dir=%1
-echo "check config"
+echo "check config" %ERRORLEVEL%
 for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "^dn_rpc_port"
 %superior_dir%\conf\iotdb-datanode.properties') do (
 set dn_rpc_port=%%i
@@ -10,12 +10,13 @@ for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "dn_rpc_address"
 %superior_dir%\conf\iotdb-datanode.properties') do (
 set dn_rpc_address=%%i
 )
-echo "netstat"
+echo "netstat" %ERRORLEVEL%
 netstat /ano | findstr %dn_rpc_address%:%dn_rpc_port%
-echo "check port"
+echo "check port" %ERRORLEVEL%
 for /f "tokens=5" %%a in ('netstat /ano ^| findstr %dn_rpc_address%:%dn_rpc_port%') do (
 echo "PID is %%a, stop datanode failed. exit."
 exit 1
 )
-echo "finally"
+echo "finally" %ERRORLEVEL%
 echo "stop datanode succeed. continue."
+@REM exit /B
