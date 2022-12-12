4d7a5515992faf72d77c3c88aec6762e221ffdb5
xiaoyekanren <876670773@qq.com>
2022-12-12 18:24:14 +0800

    增加新脚本用于拿到端口信息 v2

--- check_scripts/step_0_get_path.bat
+++ check_scripts/step_0_get_path.bat
@@ -2,7 +2,12 @@
 
 set superior_dir=%1
 
+echo "get cn_internal_port"
 for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "^cn_internal_port" %superior_dir%\conf\iotdb-confignode.properties') do (  set cn_internal_port=%%i )
+echo "get cn_internal_address"
 for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "^cn_internal_address" %superior_dir%\conf\iotdb-confignode.properties') do (  set cn_internal_address=%%i )
+echo "get dn_rpc_port"
 for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "^dn_rpc_port" %superior_dir%\conf\iotdb-datanode.properties') do (  set dn_rpc_port=%%i )
+echo "get dn_rpc_address"
 for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "^dn_rpc_address" %superior_dir%\conf\iotdb-datanode.properties') do (  set dn_rpc_address=%%i )
+echo "get over"
