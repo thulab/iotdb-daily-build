7379f3dc8b44debc0fd62c8e7cfe0095eab4bb56
xiaoyekanren <876670773@qq.com>
2023-03-09 00:23:29 +0800

    daily

--- action-release-userguide/program/test_release_master.py
+++ action-release-userguide/program/test_release_master.py
@@ -4,26 +4,32 @@ import format_md_img
 import sys
 
 
-def format_md_path(string):
-    return string.replace(' ', '\ ')
+def format_md_path(md_list):
+    format_md_list = []
+    for md in md_list:
+        format_md_list.append(md.replace(' ', '\ '))
+    return format_md_list
 
 
 def get_master_md_list(iotdb_path):
+    key = ''
     md_file_dict = dict(parse_json_generate_file_list.generate_different_version_md_dict(iotdb_path))
     for key in md_file_dict.keys():
         if not 'master' in key:
             continue
         else:
             break
-    print(key)
-    print(md_file_dict.get(key))
-    return key, md_file_dict.get(key)
+    if not key:
+        print('error: zhaobudao ')
+    print('info: 找到了master配置参数.')
+    return key, format_md_path(md_file_dict.get(key))
 
 
 def main(iotdb_path):
     title, master_md_list = get_master_md_list(iotdb_path)
-    format_md_img.(master_md_list)
-
+    print(title)
+    print(master_md_list)
+    format_md_img.format_md_img(master_md_list)
 
 
 if __name__ == '__main__':
