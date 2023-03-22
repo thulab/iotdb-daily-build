daf5667cb309e33742ce0e50c811efa91867f50e
xiaoyekanren <876670773@qq.com>
2023-03-22 10:32:44 +0800

    new version

--- action-release-userguide/program/test_release_master.py
+++ action-release-userguide/program/test_release_master.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import os.path
-
+import subprocess
 import parse_json_generate_file_list
 import format_md_img
 import sys
@@ -14,10 +14,11 @@ def get_master_md_list(iotdb_path):
         if 'master' not in key:
             continue
         else:
+            print('info: 找到了master配置参数.')
             break
     if not key:
-        print('error: zhaobudao ')
-    print('info: 找到了master配置参数.')
+        print('error: 找不到 master 分支 ')
+        exit()
     return key, common.replace_space_add_backslash(md_file_dict.get(key))
 
 
@@ -29,6 +30,7 @@ def main(iotdb_path):
     title, master_md_list = get_master_md_list(iotdb_path)  # 拿到master分支的标题，markdown文件列表
     user_guide_abs_path = generate_user_guide_abs_path(iotdb_home)  # 拼接出来userguide的绝对值路径
     format_md_img.main(user_guide_abs_path)  # 执行下一个文件的操作
+    subprocess.getoutput('pandoc -f markdown -t docx -o result.docx --reference-doc Templates/template_iotdb.docx --log=log.txt %s' % ','.join(master_md_list))
 
 
 if __name__ == '__main__':
